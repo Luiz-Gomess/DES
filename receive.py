c3b1532c44b6f64a8d50bb5639a52aee315315cf
LucasKaiquee <lucaskaique743@gmail.com>
2025-02-03 21:09:12 -0300

    feat: add um servidor socket nos arquivos de sender e receive

---
+++ receive.py
@@ -0,0 +1,60 @@
+from diffie_hellman import Diffie_Hellman
+from des import DES
+import socket
+
+# Configuração do socket
+host = 'localhost'
+port = 12345
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Conecta ao servidor
+    sock.connect((host, port))
+    print("Conectado ao servidor")
+
+    print("------------------------------------------")
+
+    print("Diffie-Hellman:")
+
+    # cria uma instância da classe Diffie_Hellman
+    receive = Diffie_Hellman(n=23, g=5)
+    print("Chave privada do receive: ", receive.get_private_key())
+
+    # gera a chave pública do receive
+    receive.generate_public_key()
+    public_key_receive = receive.get_public_key()
+    print(f"Chave pública do receive: {public_key_receive}")
+
+    # Envia a chave pública do receive
+    sock.sendall(str(public_key_receive).encode('utf-8'))
+    print("Chave pública do receive enviada")
+
+    # Aguarda a chave pública do sender
+    print("Aguardando chave pública do sender...")
+    public_key_receive = sock.recvfrom(1024)
+    print(f"Chave pública do sender: {public_key_receive[0].decode('utf-8')}")
+
+    # Calcula o segredo compartilhado
+    receive.calculate_shared_secret(int(public_key_receive[0].decode('utf-8')))
+    shared_secret = receive.get_shared_secret()
+    print(f"Segredo compartilhado: {shared_secret}")
+
+    print("------------------------------------------")
+
+    # cria uma instâcia da classe DES
+    des = DES()
+    print("Descriptografia:")
+
+    # Aguarda a mensagem cifrada
+    print("Aguardando mensagem cifrada...")
+    ciphertext = sock.recvfrom(1024)[0]
+    print(f"Mensagem cifrada recebida: {ciphertext}")
+
+    # Descriptografa a mensagem
+    plaintext = des.decrypt(ciphertext, str(shared_secret))
+    print(f"Mensagem decifrada: {plaintext}")
+
+finally:
+    # Fecha a conexão
+    sock.close()
+    print("Conexão fechada")
---
+++ sender.py
@@ -0,0 +1,68 @@
+from des import DES
+from diffie_hellman import Diffie_Hellman
+import socket
+
+# Configuração do socket
+host = 'localhost'
+port = 12345
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Vincula o socket ao endereço e porta
+sock.bind((host, port))
+
+# Escuta por conexões
+sock.listen(1)
+print("Aguardando conexão...")
+
+# Aceita a conexão de um cliente
+conn, addr = sock.accept()
+print(f"Conectado ao cliente: {addr}")
+
+try:
+    print("------------------------------------------")
+
+    print("Diffie-Hellman:")
+
+    # cria uma instância da classe Diffie_Hellman
+    
+    sender = Diffie_Hellman(n=23, g=5)
+    print("Chave privada do sender: ", sender.get_private_key())
+
+    # gera a chave pública do sender
+    sender.generate_public_key()
+    public_key_sender = sender.get_public_key()
+    print(f"Chave pública do sender: {public_key_sender}")
+
+    # Envia a chave pública do sender
+    conn.sendall(str(public_key_sender).encode('utf-8'))
+    print("Chave pública do sender enviada")
+
+    # Aguarda a chave pública do receiver
+    print("Aguardando chave pública do receiver...")
+    public_key_receive = conn.recv(1024)
+    print(f"Chave pública do receiver: {public_key_receive.decode('utf-8')}")
+
+    # Calcula o segredo compartilhado
+    sender.calculate_shared_secret(int(public_key_receive.decode('utf-8')))
+    shared_secret = sender.get_shared_secret()
+    print(f"Segredo compartilhado: {shared_secret}")
+
+    print("------------------------------------------")
+
+    # cria uma instância da classe DES
+    des = DES()
+    print("Criptografia:")
+    plaintext = input("Digite a mensagem a ser cifrada: ")
+
+    # Cifra a mensagem
+    ciphertext = des.encrypt(plaintext, str(shared_secret))
+    print(f"Mensagem cifrada: {ciphertext}")
+
+    # Envia a mensagem cifrada
+    conn.sendall(ciphertext)
+    print("Mensagem cifrada enviada")
+
+finally:
+    # Fecha a conexão
+    conn.close()
+    print("Conexão fechada")
